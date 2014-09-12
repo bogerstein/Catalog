8c709183248ed2f76ad110b6d932c09f18d60450
Dan Bogert <dan.bogert@gmail.com>
2014-09-11 21:14:47 -0400

    stuff goes here

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,13 @@
+/*
+
+$("div").hover(function(){
+	if (!$(this).hasClass('animated')) {
+		$(this).dequeue().stop().animate({ width: "200px" });
+	}
+}, function() {
+    $(this).addClass('animated').animate({ width: "100px" }, "normal", "linear", function() {
+		$(this).removeClass('animated').dequeue();
+	});
+});
+
+*/
